4351a26053d525d2bf9d44fe668a47ebae2b8b30
Vincent Berthiaume <vberthiaume@gmail.com>
2025-01-23 18:28:13 -0500

    actually make the screen work 🤯

--- esp32s3Blink/src/main.cpp
+++ esp32s3Blink/src/main.cpp
@@ -1,4 +1,8 @@
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 // How many internal neopixels do we have? some boards have more than one!
 #define NUMPIXELS 1
@@ -9,17 +13,33 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 void setup()
 {
   Serial.begin(115200);
+  // this is to wait for the serial monitor!
+  // while (! Serial)
+  //   delay(10);
 
+  //turn on the led
 #if defined(NEOPIXEL_POWER)
-  // If this board has a power control pin, we must set it to output and high
-  // in order to enable the NeoPixels. We put this in an #if defined so it can
-  // be reused for other boards without compilation errors
   pinMode(NEOPIXEL_POWER, OUTPUT);
   digitalWrite(NEOPIXEL_POWER, HIGH);
 #endif
 
   pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   pixels.setBrightness(20); // not so bright
+
+  // turn on the tft backlight
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  tft.init(135, 240);             // Init ST7789 240x135
+  tft.setRotation(3);             // rotates the screen
+  tft.fillScreen(ST77XX_BLACK);   // fills the screen with black colour
+  tft.setCursor(10, 10);          // starts to write text at y10 x10
+  tft.setTextColor(ST77XX_MAGENTA); // text colour to white you can use hex codes like 0xDAB420 too
+  tft.setTextSize(3);             // sets font size
+  tft.setTextWrap(true);
+  tft.print("LAURA & ZOEY\n");
+  tft.setTextSize(2); // sets font size
+  tft.print("sont des patates!");
 }
 
 // the loop routine runs over and over again forever:
@@ -29,7 +49,7 @@ void loop()
   Serial.println("Hello!");
 
   // set color to red
-  pixels.fill(0xFF0000);
+  pixels.fill(ST77XX_MAGENTA);
   pixels.show();
   delay(500); // wait half a second
 
