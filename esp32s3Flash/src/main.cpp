ac89ddaf2c064fb1a2b5a2ecf5f67746ce833caa
Vincent Berthiaume <vberthiaume@gmail.com>
2025-01-23 17:08:08 -0500

    esp32-s3 can now blink 🥹

--- esp32s3Flash/src/main.cpp
+++ esp32s3Flash/src/main.cpp
@@ -1,20 +1,40 @@
-#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-#define LED 5
+// How many internal neopixels do we have? some boards have more than one!
+#define NUMPIXELS 1
 
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// the setup routine runs once when you press reset:
 void setup()
 {
-  pinMode(LED, OUTPUT);
-
   Serial.begin(115200);
-  Serial.println("Hello, Wokwi!");
+
+#if defined(NEOPIXEL_POWER)
+  // If this board has a power control pin, we must set it to output and high
+  // in order to enable the NeoPixels. We put this in an #if defined so it can
+  // be reused for other boards without compilation errors
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
+#endif
+
+  pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(20); // not so bright
 }
 
+// the loop routine runs over and over again forever:
 void loop()
 {
-  digitalWrite(LED, LOW);
-  delay(1000);
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  Serial.println("ducoup");
+  // say hi
+  Serial.println("Hello!");
+
+  // set color to red
+  pixels.fill(0xFF0000);
+  pixels.show();
+  delay(500); // wait half a second
+
+  // turn off
+  pixels.fill(0x000000);
+  pixels.show();
+  delay(500); // wait half a second
 }
