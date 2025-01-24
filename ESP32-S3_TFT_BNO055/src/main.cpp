c97a811a734e821a6e65e1795147006347d533fc
Vincent Berthiaume <vberthiaume@gmail.com>
2025-01-24 14:40:02 -0500

    move tft + bno code to the right folder

--- ESP32-S3_TFT_BNO055/src/main.cpp
+++ ESP32-S3_TFT_BNO055/src/main.cpp
@@ -1,120 +1,59 @@
-constexpr int esp32s3_CS{7};
-constexpr int esp32s3_DC{39};
-constexpr int esp32s3_SCL{41};
-constexpr int esp32s3_SDA{42};
+// ======== tft screen ========
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
-//========================= setup screen =======================
-#include <Adafruit_GFX.h>     // Core graphics library
-#include <Adafruit_ILI9341.h> // Hardware-specific library for ST7789
-#include <SPI.h>
+// ======== BNO IMU ========
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+Adafruit_BNO055 bno = Adafruit_BNO055();
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(esp32s3_CS, esp32s3_DC);
-
-//======================== setup MPU6050 =======================
-#include <Adafruit_MPU6050.h>
-Adafruit_MPU6050 mpu;
-
-float prevAccelX = 0, prevAccelY = 0, prevAccelZ = 0;
-float prevGyroX = 0, prevGyroY = 0, prevGyroZ = 0;
-float prevTemp = 0;
-
-
-void setup() {
-  //general stuff
+// the setup routine runs once when you press reset:
+void setup()
+{
   Serial.begin(115200);
-  Serial.println("Beginning setup");
 
-  Wire.setPins(esp32s3_SDA, esp32s3_SCL);
-  Wire.begin();
+  //=== turn on and init the tft screen ===
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+  tft.init(135, 240);               // Init ST7789 240x135
+  tft.fillScreen(ST77XX_BLACK);     // Clear the screen
+  tft.setRotation(3);               // rotates the screen
+  tft.setTextColor(ST77XX_MAGENTA); // text colour to white you can use hex codes like 0xDAB420 too
+  tft.setTextSize(3);               // sets font size. 1 is default 6x8, 2 is 12x16, 3 is 18x24, etc
 
-  //init screen
-  tft.begin();
-  Serial.println(F("Screen is Initialized"));
+  //=== init BNO IMU ===
+  if (!bno.begin(OPERATION_MODE_IMUPLUS))
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.println("No BNO055 detected... Check your wiring or I2C ADDR!");
+    while (1)
+      ;
+  }
 
-  //init IMU
-  if (!mpu.begin())
-    Serial.println("Failed to find MPU6050 chip");
-  else
-    Serial.println("MPU6050 chip is initialized");
+  delay(1000);
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.println("MPU6050 chip is setup");
-
-  tft.setCursor(26, 120);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(3);
-  tft.println("Hello, TFT!");
-
-  tft.setCursor(20, 160);
-  tft.setTextColor(ILI9341_GREEN);
-  tft.setTextSize(2);
-  tft.println("I can has colors?");
-
-  delay(2000);
-
-  Serial.println("setup completed");
-  tft.fillScreen(ILI9341_BLACK);
+  bno.setExtCrystalUse(true);
+  Serial.println("setup completed successfully");
 }
-
-void printSensorData()
-{
-    // Clear the area and update the screen
-    tft.fillRect(0, 0, 240, 60, ILI9341_BLACK);
-    tft.setCursor(0, 0);
-    tft.setTextColor(ILI9341_WHITE);
-    tft.setTextSize(1);
-    tft.print("Accel X: ");
-    tft.print(prevAccelX);
-    tft.print(" m/s^2\n");
-    tft.print("Accel Y: ");
-    tft.print(prevAccelY);
-    tft.print(" m/s^2\n");
-    tft.print("Accel Z: ");
-    tft.print(prevAccelZ);
-    tft.print(" m/s^2\n");
-
-    tft.print("Gyro X: ");
-    tft.print(prevGyroX);
-    tft.print(" rad/s\n");
-    tft.print("Gyro Y: ");
-    tft.print(prevGyroY);
-    tft.print(" rad/s\n");
-    tft.print("Gyro Z: ");
-    tft.print(prevGyroZ);
-    tft.print(" rad/s\n");
-
-    tft.print("Temp: ");
-    tft.print(prevTemp);
-    tft.print(" C\n");
-}
-
-void updateSensorData()
-{
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-
-    // Check if the sensor data has changed
-    if (a.acceleration.x != prevAccelX || a.acceleration.y != prevAccelY || a.acceleration.z != prevAccelZ ||
-        g.gyro.x != prevGyroX || g.gyro.y != prevGyroY || g.gyro.z != prevGyroZ ||
-        temp.temperature != prevTemp)
-    {
-        // Update cached values
-        prevAccelX = a.acceleration.x;
-        prevAccelY = a.acceleration.y;
-        prevAccelZ = a.acceleration.z;
-        prevGyroX = g.gyro.x;
-        prevGyroY = g.gyro.y;
-        prevGyroZ = g.gyro.z;
-        prevTemp = temp.temperature;
-
-        printSensorData();
-    }
-}
-
 
 void loop()
 {
-  updateSensorData();
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  /* Display the floating point data */
+  tft.fillScreen(ST77XX_BLACK); // Clear the screen
+  tft.setCursor(0, 10);
+  tft.print("X: ");
+  tft.print(event.orientation.x, 4);
+  tft.print("\nY: ");
+  tft.print(event.orientation.y, 4);
+  tft.print("\nZ: ");
+  tft.print(event.orientation.z, 4);
+  tft.println("");
+  delay(200);
 }
--- esp32s3Blink/src/main.cpp
+++ esp32s3Blink/src/main.cpp
@@ -1,7 +1,25 @@
+#define PRINT_TO_TFT 1
+
 // ======== neopixel led ========
+#if !PRINT_TO_TFT
 #include <Adafruit_NeoPixel.h>
 #define NUMPIXELS 1
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// array of colors to cycle through
+uint32_t colors[] = {
+    pixels.Color(255, 0, 0),     // Red
+    pixels.Color(0, 255, 0),     // Green
+    pixels.Color(0, 0, 255),     // Blue
+    pixels.Color(255, 255, 0),   // Yellow
+    pixels.Color(0, 255, 255),   // Cyan
+    pixels.Color(255, 0, 255),   // Magenta
+    pixels.Color(255, 255, 255), // White
+    pixels.Color(128, 0, 128)    // Purple
+};
+
+int curColor = -1;
+#endif
 
 // ======== tft screen ========
 #include <Adafruit_GFX.h>    // Core graphics library
@@ -19,30 +37,32 @@ Adafruit_BNO055 bno = Adafruit_BNO055();
 void setup()
 {
   Serial.begin(115200);
+#if !PRINT_TO_TFT
   while (! Serial)
     delay(10);//wait for serial monitor to open
 
   //=== init the neopixel led ===
-#if defined(NEOPIXEL_POWER)
   pinMode(NEOPIXEL_POWER, OUTPUT);
   digitalWrite(NEOPIXEL_POWER, HIGH);
+  pixels.begin();
+  pixels.setBrightness(20);
 #endif
-
-  pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels.setBrightness(20); // not so bright
 
   //=== turn on and init the tft screen ===
   pinMode(TFT_BACKLITE, OUTPUT);
   digitalWrite(TFT_BACKLITE, HIGH);
-
   tft.init(135, 240);               // Init ST7789 240x135
   tft.setRotation(3);               // rotates the screen
-  tft.fillScreen(ST77XX_BLACK);     // fills the screen with black colour
-  tft.setCursor(10, 10);            // starts to write text at y10 x10
   tft.setTextColor(ST77XX_MAGENTA); // text colour to white you can use hex codes like 0xDAB420 too
   tft.setTextSize(3);               // sets font size. 1 is default 6x8, 2 is 12x16, 3 is 18x24, etc
+
+#if !PRINT_TO_TFT 
+  //greet the world
+  tft.fillScreen(ST77XX_BLACK);     // fills the screen with black colour
+  tft.setCursor(10, 10);            // starts to write text at y10 x10
   tft.setTextWrap(true);
   tft.print("Hello World!\n");
+#endif
 
   //=== init BNO IMU ===
   if (!bno.begin(OPERATION_MODE_IMUPLUS))
@@ -59,33 +79,34 @@ void setup()
   Serial.println("setup completed successfully");
 }
 
-// array of colors to cycle through
-uint32_t colors[] = {
-    pixels.Color(255, 0, 0),     // Red
-    pixels.Color(0, 255, 0),     // Green
-    pixels.Color(0, 0, 255),     // Blue
-    pixels.Color(255, 255, 0),   // Yellow
-    pixels.Color(0, 255, 255),   // Cyan
-    pixels.Color(255, 0, 255),   // Magenta
-    pixels.Color(255, 255, 255), // White
-    pixels.Color(128, 0, 128)    // Purple
-};
-
-int i = -1;
 void loop()
 {
+#if !PRINT_TO_TFT
   //have the led cycle through the colors
-  if (++i == 8)
-    i = 0;
+  if (++curColor == 8)
+    curColor = 0;
 
-  pixels.fill(colors[i]);
+  pixels.fill(colors[curColor]);
   pixels.show();
+#endif
 
   /* Get a new sensor event */
   sensors_event_t event;
   bno.getEvent(&event);
 
   /* Display the floating point data */
+#if PRINT_TO_TFT
+  tft.fillScreen(ST77XX_BLACK); // Clear the screen
+  tft.setCursor(10, 10);
+  tft.print("X: ");
+  tft.print(event.orientation.x, 4);
+  tft.print("\nY: ");
+  tft.print(event.orientation.y, 4);
+  tft.print("\nZ: ");
+  tft.print(event.orientation.z, 4);
+  tft.println("");
+  delay(200);
+#else
   Serial.print("X: ");
   Serial.print(event.orientation.x, 4);
   Serial.print("\tY: ");
@@ -93,6 +114,6 @@ void loop()
   Serial.print("\tZ: ");
   Serial.print(event.orientation.z, 4);
   Serial.println("");
-
   delay(100);
+#endif
 }
