74e8348541372728985cc424a9c0cc3982022dc9
Vincent Berthiaume <vberthiaume@gmail.com>
2025-01-24 14:54:20 -0500

    ok now both esp32s3 projects do what they are called

--- esp32s3Blink/src/main.cpp
+++ esp32s3Blink/src/main.cpp
@@ -1,10 +1,46 @@
-#define PRINT_TO_TFT 1
-
-// ======== neopixel led ========
-#if !PRINT_TO_TFT
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// How many internal neopixels do we have? some boards have more than one!
 #define NUMPIXELS 1
+
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// the setup routine runs once when you press reset:
+void setup()
+{
+  Serial.begin(115200);
+  // this is to wait for the serial monitor!
+  // while (! Serial)
+  //   delay(10);
+
+  // turn on the led
+#if defined(NEOPIXEL_POWER)
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
+#endif
+
+  pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(20); // not so bright
+
+  // turn on the tft backlight
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  tft.init(135, 240);               // Init ST7789 240x135
+  tft.setRotation(3);               // rotates the screen
+  tft.fillScreen(ST77XX_BLACK);     // fills the screen with black colour
+  tft.setCursor(0, 10);            // starts to write text at y10 x10
+  tft.setTextColor(ST77XX_MAGENTA); // text colour to white you can use hex codes like 0xDAB420 too
+  tft.setTextSize(3);               // sets font size. 1 is default 6x8, 2 is 12x16, 3 is 18x24, etc
+  tft.setTextWrap(true);
+  tft.print("HELLO WORLD!\n");
+  tft.setTextSize(1); // sets font size
+  tft.print("hello world but way smaller");
+}
 
 // array of colors to cycle through
 uint32_t colors[] = {
@@ -18,102 +54,20 @@ uint32_t colors[] = {
     pixels.Color(128, 0, 128)    // Purple
 };
 
-int curColor = -1;
-#endif
-
-// ======== tft screen ========
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-
-// ======== BNO IMU ========
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
-Adafruit_BNO055 bno = Adafruit_BNO055();
-
-// the setup routine runs once when you press reset:
-void setup()
-{
-  Serial.begin(115200);
-#if !PRINT_TO_TFT
-  while (! Serial)
-    delay(10);//wait for serial monitor to open
-
-  //=== init the neopixel led ===
-  pinMode(NEOPIXEL_POWER, OUTPUT);
-  digitalWrite(NEOPIXEL_POWER, HIGH);
-  pixels.begin();
-  pixels.setBrightness(20);
-#endif
-
-  //=== turn on and init the tft screen ===
-  pinMode(TFT_BACKLITE, OUTPUT);
-  digitalWrite(TFT_BACKLITE, HIGH);
-  tft.init(135, 240);               // Init ST7789 240x135
-  tft.setRotation(3);               // rotates the screen
-  tft.setTextColor(ST77XX_MAGENTA); // text colour to white you can use hex codes like 0xDAB420 too
-  tft.setTextSize(3);               // sets font size. 1 is default 6x8, 2 is 12x16, 3 is 18x24, etc
-
-#if !PRINT_TO_TFT 
-  //greet the world
-  tft.fillScreen(ST77XX_BLACK);     // fills the screen with black colour
-  tft.setCursor(10, 10);            // starts to write text at y10 x10
-  tft.setTextWrap(true);
-  tft.print("Hello World!\n");
-#endif
-
-  //=== init BNO IMU ===
-  if (!bno.begin(OPERATION_MODE_IMUPLUS))
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.println("No BNO055 detected... Check your wiring or I2C ADDR!");
-    while (1)
-      ;
-  }
-
-  delay(1000);
-
-  bno.setExtCrystalUse(true);
-  Serial.println("setup completed successfully");
-}
-
 void loop()
 {
-#if !PRINT_TO_TFT
-  //have the led cycle through the colors
-  if (++curColor == 8)
-    curColor = 0;
+  // cycle through colors
+  for (int i = 0; i < 8; i++)
+  {
+    pixels.fill(colors[i]);
+    pixels.show();
+    delay(500); // wait half a second
+  }
 
-  pixels.fill(colors[curColor]);
+  // turn off
+  pixels.fill(0x000000);
   pixels.show();
-#endif
+  delay(500); // wait half a second
 
-  /* Get a new sensor event */
-  sensors_event_t event;
-  bno.getEvent(&event);
-
-  /* Display the floating point data */
-#if PRINT_TO_TFT
-  tft.fillScreen(ST77XX_BLACK); // Clear the screen
-  tft.setCursor(10, 10);
-  tft.print("X: ");
-  tft.print(event.orientation.x, 4);
-  tft.print("\nY: ");
-  tft.print(event.orientation.y, 4);
-  tft.print("\nZ: ");
-  tft.print(event.orientation.z, 4);
-  tft.println("");
-  delay(200);
-#else
-  Serial.print("X: ");
-  Serial.print(event.orientation.x, 4);
-  Serial.print("\tY: ");
-  Serial.print(event.orientation.y, 4);
-  Serial.print("\tZ: ");
-  Serial.print(event.orientation.z, 4);
-  Serial.println("");
-  delay(100);
-#endif
+  Serial.print(".");
 }
