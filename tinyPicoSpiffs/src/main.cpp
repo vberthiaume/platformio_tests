67d3ccb6ec24fb651365e1ad1337ce22cdf3571a
Vincent Berthiaume <vberthiaume@gmail.com>
2025-02-19 10:36:10 -0500

    cleanup

--- tinyPicoSpiffs/src/main.cpp
+++ tinyPicoSpiffs/src/main.cpp
@@ -1,9 +1,6 @@
-#include <TinyPICO.h>
+// #include <TinyPICO.h>
 #include "SPIFFS.h"
 
-#define LED_PIN 25 // Pin connected to the LED anode
-
-TinyPICO tp;
 String fileContent;
 
 void setup()
@@ -27,24 +24,9 @@ void setup()
     fileContent += (char)file.read();
 
   file.close();
-
-  // Initialize LED pin as output
-  pinMode(LED_PIN, OUTPUT);
-
-  tp.DotStar_Clear();
-  tp.DotStar_SetBrightness(255);
 }
 
 void loop()
 {
-  // Turn the LED on (full brightness)
-  analogWrite(LED_PIN, 255);
-  delay(250); // Keep it on for 1 second
-
-  // Turn the LED off
-  analogWrite(LED_PIN, 0);
-  delay(250); // Keep it off for 1 second
-
-  tp.DotStar_CycleColor(10);
   Serial.println(fileContent);
 }
